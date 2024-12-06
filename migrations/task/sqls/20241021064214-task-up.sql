7f7794b679090d685138b84c664c798f8f371f0c
chun <>
2024-12-07 02:07:38 +0800

    update 2-2(1)

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -81,10 +81,10 @@ INSERT INTO "CREDIT_PACKAGE" (name, credit_amount, price) values
 --1.
 -寫入購買資料
 insert into "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) values
-((select id from "USER" where email = 'wXlTq@hexschooltest.io'),
-  (select id from "CREDIT_PACKAGE" where name = '14 堂組合包方案'),
-  (select credit_amount from "CREDIT_PACKAGE" where name = '14 堂組合包方案'),
-  (select price from "CREDIT_PACKAGE" where name = '14 堂組合包方案'));
+((select id from "USER" WHERE email = 'wXlTq@hexschooltest.io'),
+  (select id from "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+  (select credit_amount from "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+  (select price from "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'));
 
 
 
