0f82aabc16151f83c0afe30c9a33952f6489eaa6
chun <>
2024-12-07 02:02:12 +0800

    update 2-2(1)

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -81,14 +81,13 @@ INSERT INTO "CREDIT_PACKAGE" (name, credit_amount, price) values
 --1.
 -寫入購買資料
 INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) values
---用email取出會員id
-((select id from "USER" WHERE email ='wXlTq@hexschooltest.io' ),
---用name 取出組合包id
-(select id from "CREDIT_PACKAGE" WHERE name ='14 堂組合包方案'),
---用name 取出組合包的堂數
-(select credit_amount from "CREDIT_PACKAGE" WHERE name ='14 堂組合包方案'),
---用name 取出組合包的價格
-(select price from "CREDIT_PACKAGE" WHERE name ='14 堂組合包方案'));
+((select id from "USER" WHERE email ='wXlTq@hexschooltest.io' ),--用email取出會員id
+(select id from "CREDIT_PACKAGE" WHERE name ='14 堂組合包方案'),--用name 取出組合包id
+(select credit_amount from "CREDIT_PACKAGE" WHERE name ='14 堂組合包方案'),--用name 取出組合包的堂數
+
+(select price from "CREDIT_PACKAGE" WHERE name ='14 堂組合包方案'));--用name 取出組合包的價格
+
+
 
 
 -- ████████  █████   █    ████   
